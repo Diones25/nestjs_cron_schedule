aea5d12a2cafb35042f7193e947fb8d65cc239aa
Diones <pereiradiones987@gmail.com>
2026-03-27 16:09:49 -0300

    feat: modulo Task

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    TaskModule,
   ],
   controllers: [],
   providers: [],
