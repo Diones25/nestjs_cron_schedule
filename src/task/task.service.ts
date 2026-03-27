ca8e6bc0d9c745973276bfc8d9bca74034e96226
Diones <pereiradiones987@gmail.com>
2026-03-27 16:33:34 -0300

    feat: script dev

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,26 +1,24 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class TaskService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
-  }
-
-  findAll() {
-    return `This action returns all task`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
-  }
-
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: Logger,
+  ) {}
+  
+  /**
+   * Agendamento para ser executado a cada 50 minutos.
+   * Expressão cron:
+   * Minuto (0-59) | Hora (0-23) | Dia do Mês (1-31) | Mês (1-12) | Dia da Semana (0-7)
+   * 50          * * * *
+   *
+   * Isso significa: A cada 50 minutos de cada hora, todos os dias.
+  */
+  @Cron('*/50 * * * *')
+  async handleCronKeepAlive() {
+    this.logger.log('Iniciando requisição POST para JSONPlaceholder...');
   }
 }
