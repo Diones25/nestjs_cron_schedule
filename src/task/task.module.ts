995f4490bde520245b02a439aa9fa58782f7f65d
Diones <pereiradiones987@gmail.com>
2026-03-27 16:59:32 -0300

    feat: removendo controller não utilizada

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
-import { TaskController } from './task.controller';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule],
-  controllers: [TaskController],
+  controllers: [],
   providers: [TaskService],
 })
 export class TaskModule {}
