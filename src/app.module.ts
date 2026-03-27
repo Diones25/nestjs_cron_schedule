c8e08f6c34f8b1083f10a7e74411843ed2573ccd
Diones <pereiradiones987@gmail.com>
2026-03-27 16:25:00 -0300

    feat: configurando log winston

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TaskModule } from './task/task.module';
 import { HttpModule } from '@nestjs/axios';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            winston.format.colorize(),
+            winston.format.printf(({ timestamp, level, message, ms }) => {
+              return `${timestamp} [${level}] ${message} (${ms})`;
+            }),
+          )
+        }),
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+      ]
+    }),
     HttpModule,
     TaskModule,
   ],
