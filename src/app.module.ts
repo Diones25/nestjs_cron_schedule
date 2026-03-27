8a90d066034a6103c004ba09bf807944c3e099a3
Diones <pereiradiones987@gmail.com>
2026-03-27 16:07:24 -0300

    feat: Configuração do Módulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
